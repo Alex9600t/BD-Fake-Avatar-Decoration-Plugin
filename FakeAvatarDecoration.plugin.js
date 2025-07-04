fd84cd3a9e2ebcff54a2fbc90d22a0a2f5df7231
Alex9600t <dukovaleks2@gmail.com>
2025-07-04 22:03:48 +0300

    feat(mirror): Added mirror src

--- FakeAvatarDecoration.plugin.js
+++ FakeAvatarDecoration.plugin.js
@@ -5,53 +5,104 @@
  * @website https://vcfad.vercel.app/bd/
  * @source https://github.com/Alex9600t/BD-Fake-Avatar-Decoration-Plugin
  * @description Simple plugin to add a fake Avatar decoration visible to all users who have the plugin installed.
- * @version 0.0.1
+ * @version 0.0.2
  */
 
+let observe_44b0c;
+let observe_c19a55;
+// -----Main-----
 module.exports = class FAD {
     start() {
-        init()
+        init();
+        run();
     }
-    stop() {}
-    getSettingsPanel () {
-			let template = document.createElement("template");
-			template.innerHTML = `<div>${settings()}</div>`;
-			return template.content.firstElementChild;
-		}
-}
 
-function vc_fad_input() {
-    const data = document.querySelector('.vc_fda_bd_s_input').value;
-
-    if (data && data !== "") {
-        BdApi.Net.fetch("https://vcfad.vercel.app/setAvatarDecoration", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-            "url": `${data}`
-            }),
-            redirect: "follow"
-        })
-        .then((response) => response.json())
-        .then((result) => {
-            window.open("https://discord.com/oauth2/authorize?client_id=1387953781475377212&response_type=code&redirect_uri=https%3A%2F%2Fvcfad.vercel.app%2Ffad%2Fredirection&scope=identify", "_blank");
-        })
-        .catch((error) => console.error(error));
+    stop() {
+        end();
     }
-}
 
-function settings() {
-    const blockH2 = (el => {return `<h2 class="h5_b717a1 eyebrow_b717a1 defaultMarginh5_b717a1" style="margin-bottom: 0px" id=":r6k:">${el}</h2>`});
-    const blockSub = (el => {return `<div class="colorStandard_c9946a size14_a4e6b9 description_ddd181 formText_ddd181 marginTop8_fd297e modeDefault_ddd181">${el}</div>`});
-    const blockInput = ((b0, b1) => {return `<div class="fieldWrapper_ce6bbd">${b0}<div class="inputWrapper__0f084"><input class="inputDefault__0f084 input__0f084 vc_fda_bd_s_input" title="" note="" placeholder="e.g. a_48b8411feb1e80a69048fc65b3275b75" aria-labelledby=":r6k:" value="" name=""></div>${b1}</div>`});
-    const blockBtn = (el => {return `<button class="bd-button bd-button-filled bd-button-color-brand bd-button-medium bd-button-grow vc_fda_bd_s_btn"><div class="bd-button-content">${el}</div></button>`});
-
-    return(`${blockH2("Update Avatar Decoration")}${blockInput(blockSub(`Enter ID. If you don’t know it, you can <a onclick='window.open("https://vcfad.vercel.app/list", "_blank")'>view all IDs</a>`), "")} <div style="border-bottom: 1px solid; margin-bottom: 20px;"></div> ${blockBtn("Publish", () => {console.log("Hey")})}`)
+    getSettingsPanel() {
+        let template = document.createElement("template");
+        template.innerHTML = `<div>${settings()}</div>`;
+        return template.content.firstElementChild;
+    }
 }
 
 function init() {
+    console.log("Hello from FAD!");
+    // c19a55
+    observe_c19a55 = update("avatar_c19a55", (el) => {
+        BdApi.Net.fetch(`https://vcfad.vercel.app/avatarDecoration/${el.src.split("/")[4]}`)
+            .then(r => r.status !== 200
+                ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${el.src.split("/")[4]}`)
+                : r
+            )
+            .then(r => r.json())
+            .then(data => {
+                if (data.status === 0) {
+                    const newDecoration = document.createElement('img');
+                    newDecoration.className = "avatarDecoration_c19a55 fad_ad";
+                    newDecoration.ariaHidden = "true";
+                    newDecoration.src = `${data.url}`;
+
+                    if (el.parentNode) {
+                        el.parentNode.insertBefore(newDecoration, el.nextSibling);
+                    }
+                }
+            })
+    });
+
+    // 44b0c
+    observe_44b0c = update("wrapper__44b0c", (el) => {
+        BdApi.Net.fetch(`https://vcfad.vercel.app/avatarDecoration/${el.querySelector('.avatar__44b0c').src.split("/")[4]}`)
+            .then(r => r.status !== 200
+                ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${el.querySelector('.avatar__44b0c').src.split("/")[4]}`)
+                : r
+            )
+            .then(r => r.json())
+            .then(data => {
+                if (data.status === 0) {
+                    const newDecoration = document.createElement('svg');
+                    newDecoration.setAttribute("width", "108");
+                    newDecoration.setAttribute("height", "96");
+                    newDecoration.setAttribute("viewBox", "0 0 108 96");
+                    newDecoration.className = "avatarDecoration__44b0c fad_ad";
+                    newDecoration.ariaHidden = "true";
+                    newDecoration.innerHTML = `<foreignObject x="0" y="0" width="96" height="96" mask="url(#svg-mask-avatar-decoration-status-round-80)">
+                                                <div class="avatarStack__44b0c"><img class="avatar__44b0c" alt=" " aria-hidden="true" src="${data.url}" as="image">
+                                                </div></foreignObject>`;
+
+                    el.appendChild(newDecoration);
+                }
+            })
+    })
+}
+
+function end() {
+    document.removeEventListener('click', (el) => {
+        let tmp = el.target;
+        if (Array.from(el.target.classList).find(className => className.startsWith('vc_fda_bd_s_btn'))) {
+            vc_fad_input()
+        } else {
+            for (let i = 0; i < 3; i++) {
+                if (Array.from(tmp.parentNode.classList).find(className => className.startsWith('vc_fda_bd_s_btn'))) {
+                    vc_fad_input();
+                    break;
+                } else {
+                    tmp = tmp.parentNode;
+                }
+            }
+        }
+    });
+
+    document.querySelectorAll('.fad_ad').forEach(el => {
+        el.remove();
+    });
+    observe_44b0c.disconnect();
+    observe_c19a55.disconnect();
+}
+
+function run() {
     document.addEventListener('click', (el) => {
         let tmp = el.target;
         if (Array.from(el.target.classList).find(className => className.startsWith('vc_fda_bd_s_btn'))) {
@@ -72,9 +123,9 @@ function init() {
     const ad01 = document.querySelector('.avatar__37e49');
     BdApi.Net.fetch(`https://vcfad.vercel.app/avatarDecoration/${ad01.querySelector('.avatar__44b0c').src.split("/")[4]}`)
         .then(r => r.status !== 200
-                    ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${ad01.querySelector('.avatar__44b0c').src.split("/")[4]}`) 
-                    : r
-                )
+            ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${ad01.querySelector('.avatar__44b0c').src.split("/")[4]}`)
+            : r
+        )
         .then(r => r.json())
         .then(data => {
             if (data.status === 0) {
@@ -82,7 +133,7 @@ function init() {
                 newDecoration.setAttribute("width", "57");
                 newDecoration.setAttribute("height", "48");
                 newDecoration.setAttribute("viewBox", "0 0 57 48");
-                newDecoration.className = "avatarDecoration__44b0c";
+                newDecoration.className = "avatarDecoration__44b0c fad_ad";
                 newDecoration.ariaHidden = "true";
                 newDecoration.innerHTML = `<foreignObject x="0" y="0" width="48" height="48" mask="url(#svg-mask-avatar-decoration-status-round-40)">
                                             <div class="avatarStack__44b0c"><img class="avatar__44b0c" alt=" " aria-hidden="true" src="${data.url}" as="image">
@@ -91,54 +142,31 @@ function init() {
                 ad01.appendChild(newDecoration);
             }
         })
+}
+// -----/Main-----
 
-    // c19a55
-    update("avatar_c19a55", (el) => {
-        BdApi.Net.fetch(`https://vcfad.vercel.app/avatarDecoration/${el.src.split("/")[4]}`)
-            .then(r => r.status !== 200
-                    ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${el.src.split("/")[4]}`) 
-                    : r
-                )
-            .then(r => r.json())
-            .then(data => {
-                console.log(data)
-                if (data.status === 0) {
-                    const newDecoration = document.createElement('img');
-                    newDecoration.className = "avatarDecoration_c19a55";
-                    newDecoration.ariaHidden = "true";
-                    newDecoration.src = `${data.url}`;
+function vc_fad_input() {
+    const data = document.querySelector('.vc_fda_bd_s_input').value;
 
-                    if (el.parentNode) {
-                        el.parentNode.insertBefore(newDecoration, el.nextSibling);
-                    }
-                }
+    if (data && data !== "") {
+        BdApi.Net.fetch("https://vcfad.vercel.app/setAvatarDecoration", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ "url": `${data}` }), redirect: "follow" })
+            .then((response) => response.json())
+            .then((result) => {
+                window.open("https://discord.com/oauth2/authorize?client_id=1387953781475377212&response_type=code&redirect_uri=https%3A%2F%2Fvcfad.vercel.app%2Ffad%2Fredirection&scope=identify", "_blank");
             })
-    });
+            .catch((error) => console.error(error));
+    }
+}
+// -----Settings-----
+function settings() {
+    const blockH2 = (el => { return `<h2 class="h5_b717a1 eyebrow_b717a1 defaultMarginh5_b717a1" style="margin-bottom: 0px" id=":r6k:">${el}</h2>` });
+    const blockSub = (el => { return `<div class="colorStandard_c9946a size14_a4e6b9 description_ddd181 formText_ddd181 marginTop8_fd297e modeDefault_ddd181">${el}</div>` });
+    const blockInput = ((b0, b1) => { return `<div class="fieldWrapper_ce6bbd">${b0}<div class="inputWrapper__0f084"><input class="inputDefault__0f084 input__0f084 vc_fda_bd_s_input" title="" note="" placeholder="e.g. a_48b8411feb1e80a69048fc65b3275b75" aria-labelledby=":r6k:" value="" name=""></div>${b1}</div>` });
+    const blockBtn = (el => { return `<button class="bd-button bd-button-filled bd-button-color-brand bd-button-medium bd-button-grow vc_fda_bd_s_btn"><div class="bd-button-content">${el}</div></button>` });
 
-    // 44b0c
-    update("wrapper__44b0c", (el) => {
-        BdApi.Net.fetch(`https://vcfad.vercel.app/avatarDecoration/${el.querySelector('.avatar__44b0c').src.split("/")[4]}`)
-            .then(r => r.status !== 200
-                ? BdApi.Net.fetch(`https://mirror-vcfad.vercel.app/avatarDecoration/${el.querySelector('.avatar__44b0c').src.split("/")[4]}`) 
-                : r
-            )
-            .then(r => r.json())
-            .then(data => {
-                if (data.status === 0) {
-                    const newDecoration = document.createElement('svg');
-                    newDecoration.setAttribute("width", "108");
-                    newDecoration.setAttribute("height", "96");
-                    newDecoration.setAttribute("viewBox", "0 0 108 96");
-                    newDecoration.className = "avatarDecoration__44b0c";
-                    newDecoration.ariaHidden = "true";
-                    newDecoration.innerHTML = `<foreignObject x="0" y="0" width="96" height="96" mask="url(#svg-mask-avatar-decoration-status-round-80)">
-                                                <div class="avatarStack__44b0c"><img class="avatar__44b0c" alt=" " aria-hidden="true" src="${data.url}" as="image">
-                                                </div></foreignObject>`;
-
-                    el.appendChild(newDecoration);
-                }
-            })
-    })
+    return (`${blockH2("Update Avatar Decoration")}${blockInput(blockSub(`Enter ID. If you don’t know it, you can <a onclick='window.open("https://vcfad.vercel.app/list", "_blank")'>view all IDs</a>`), blockSub(""))} 
+    ${blockBtn("Publish", () => { console.log("Hey") })}
+    <div class="colorStandard_c9946a size14_a4e6b9 description_ddd181 formText_ddd181 marginTop8_fd297e modeDefault_ddd181" style="font-size: 7px; line-height: 7px;">By clicking the <b>Publish</b> button, a Discord OAuth2 window will open. This is required to verify your identity and ensure you're authorized to make changes. We only receive your <b>Discord User ID</b>. No other personal data is collected. You can <a href='https://vcfad.vercel.app/avatardecoration' target='_blank'>view all data on the server</a>.</div>`)
 }
 
 function update(cl, callback) {
@@ -157,7 +185,7 @@ function update(cl, callback) {
                     callback(el);
                 })
             })
-            if (mt.type === "attributes" && (mt.target instanceof Element) && 
+            if (mt.type === "attributes" && (mt.target instanceof Element) &&
                 mt.target.classList.contains(`${cl}`)) {
                 callback(mt.target);
             }
@@ -170,4 +198,7 @@ function update(cl, callback) {
         attributes: true,
         attributeFilter: ["class"]
     })
+
+    return observer;
 }
+// -----/Settings-----
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Fake Avatar Decoration (Better Discord port)
+# Fake Avatar Decoration (Better Discord)
 
 ## Simple plugin to add a fake Avatar decoration visible to all users who have the plugin installed.
 
@@ -20,6 +20,6 @@
 1) Go to ***[the website](https://vcfad.vercel.app/list)*** and find a beautiful avatar decoration.
 2) Paste the **ID** into the ***ID field***.
 3) Click ***Publish*** and **confirm**.
-![deco 0](https://github.com/Alex9600t/public-data/blob/main/fad/RM_img_2_bd.png?raw=true)
+![deco 0](https://github.com/Alex9600t/public-data/blob/main/fad/RM_img_2_bd_fix_1.png?raw=true)
 > [!WARNING]
 > The request is valid for ***90 seconds*** only.
